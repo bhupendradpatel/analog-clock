88c26355886ca561c59d92235e05acd9175551c6
bhupendp <Bhupendra.Patel@amdocs.com>
2023-09-06 02:11:57 +0300

    Implemented Analog Watch using React and CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef} from 'react';
+import {degree} from './App.utils';
 import './App.css';
 
 function App() {
+  const hour = useRef(null);
+  const minute = useRef(null);
+  const second = useRef(null);
+
+  useEffect(() => {
+    const test = setInterval(() => {
+      const date = new Date();
+      const hh = date.getHours() * 30;
+      const mm = date.getMinutes() * degree;
+      const ss = date.getSeconds() * degree;
+
+      hour.current.style.transform = `rotate(${hh + (mm / 12)}deg)`;
+      minute.current.style.transform = `rotate(${mm}deg)`;
+      second.current.style.transform = `rotate(${ss}deg)`;
+    }, 1000);
+
+    return () => clearInterval(test);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'App'}>
+      <div className={'container'}>
+        <div className={'circle'}></div>
+        <div className={'circle'}></div>
+
+        <div className={'clock'}>
+          {new Array(60).fill('').map((_, index) => {
+            const rotate = `rotate(${( index + 1)  * 6}deg)`;
+            return (<div className={'graduations'} style={{transform: rotate}}>
+              <div className={'stick'}></div>
+            </div>)
+          })}
+
+          <div className={'indicator'}>
+            <div ref={hour} className={'hand hour'}></div>
+            <div ref={minute} className={'hand minute'}></div>
+            <div ref={second} className={'hand second'}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
