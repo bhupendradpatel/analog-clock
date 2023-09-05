252f3a4d456a02b4acefc35b7f5496c328027a43
bhupendp <Bhupendra.Patel@amdocs.com>
2023-09-06 02:37:29 +0300

    Updates

---
+++ precache-manifest.94ff4721b9ee78ffbcd1814a1b0c138c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ca70c6f563573a444d2",
+    "url": "/analog-clock/static/css/main.d52aac4b.chunk.css"
+  },
+  {
+    "revision": "3ca70c6f563573a444d2",
+    "url": "/analog-clock/static/js/main.930bdd13.chunk.js"
+  },
+  {
+    "revision": "083d5e20575d3d2780ae",
+    "url": "/analog-clock/static/js/runtime~main.ff2b93b3.js"
+  },
+  {
+    "revision": "99b1961c55d675e491f5",
+    "url": "/analog-clock/static/js/2.8808d590.chunk.js"
+  },
+  {
+    "revision": "f54d1ac797f637aac8ec",
+    "url": "/analog-clock/static/js/3.b359b5d6.chunk.js"
+  },
+  {
+    "revision": "fc367225b59816431a1de767f525059a",
+    "url": "/analog-clock/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/analog-clock/precache-manifest.94ff4721b9ee78ffbcd1814a1b0c138c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/analog-clock/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
