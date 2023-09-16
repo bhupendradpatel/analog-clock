f925a7c765a6938561ba4a5cf328cb276def1f82
bhupendp <Bhupendra.Patel@amdocs.com>
2023-09-17 00:08:04 +0300

    Fixed issues

--- src/App.js
+++ src/App.js
@@ -1,51 +1,44 @@
 import React, {useEffect, useRef} from 'react';
-import {degree} from './App.utils';
+import {setTimeForClock} from './App.utils';
 import './App.css';
 
 function App() {
-  const hour = useRef(null);
-  const minute = useRef(null);
-  const second = useRef(null);
+	const hour = useRef(null);
+	const minute = useRef(null);
+	const second = useRef(null);
 
-  useEffect(() => {
-    const test = setInterval(() => {
-      const date = new Date();
-      const hh = date.getHours() * 30;
-      const mm = date.getMinutes() * degree;
-      const ss = date.getSeconds() * degree;
+	useEffect(() => {
+		setTimeForClock(hour, minute, second);
 
-      hour.current.style.transform = `rotate(${hh + (mm / 12)}deg)`;
-      minute.current.style.transform = `rotate(${mm}deg)`;
-      second.current.style.transform = `rotate(${ss}deg)`;
-    }, 1000);
+		const test = setInterval(() => setTimeForClock(hour, minute, second), 1000);
 
-    return () => clearInterval(test);
-  }, []);
+		return () => clearInterval(test);
+	}, []);
 
 
-  return (
-    <div className={'App'}>
-      <div className={'container'}>
-        <div className={'circle'}></div>
-        <div className={'circle'}></div>
+	return (
+		<div className={'App'}>
+			<div className={'container'}> {/*  style={{visibility: loaded ? 'visible' : 'hidden'}} */}
+				<div className={'circle'}></div>
+				<div className={'circle'}></div>
 
-        <div className={'clock'}>
-          {new Array(60).fill('').map((_, index) => {
-            const rotate = `rotate(${( index + 1)  * 6}deg)`;
-            return (<div className={'graduations'} style={{transform: rotate}}>
-              <div className={'stick'}></div>
-            </div>)
-          })}
+				<div className={'clock'}>
+					{new Array(60).fill('').map((_, index) => {
+						const rotate = `rotate(${( index + 1)  * 6}deg)`;
+						return (<div className={'graduations'} style={{transform: rotate}}>
+							<div className={'stick'}></div>
+						</div>)
+					})}
 
-          <div className={'indicator'}>
-            <div ref={hour} className={'hand hour'}></div>
-            <div ref={minute} className={'hand minute'}></div>
-            <div ref={second} className={'hand second'}></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className={'indicator'}>
+						<div ref={hour} className={'hand hour'}></div>
+						<div ref={minute} className={'hand minute'}></div>
+						<div ref={second} className={'hand second'}></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/App.utils.js
+++ src/App.utils.js
@@ -1,1 +1,14 @@
 export const degree = 6;
+
+export const setTimeForClock = (hour, minute, second) => {
+    const date = new Date();
+    const hh = date.getHours() * 30;
+    const mm = date.getMinutes() * degree;
+    const ss = date.getSeconds() * degree;
+
+    if (hour.current || minute.current || second.current) {
+        hour.current.style.transform = `rotate(${hh + (mm / 12)}deg)`;
+        minute.current.style.transform = `rotate(${mm}deg)`;
+        second.current.style.transform = `rotate(${ss}deg)`;
+    }
+}
